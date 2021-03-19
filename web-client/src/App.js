b59d230c8f803c7c6f47befff6491fd21568360a
Doan Van Hoang <hoangusa08@gmail.com>
2021-03-19 15:04:24 +0700

    router
    create router

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import Header from "./components/header"
 import Fouter from "./components/footer"
 import Login from "./components/login"
@@ -8,9 +9,14 @@ import Cart from "./components/cart"
 function App() {
   return (
     <div >
-      <Header/>
-      <Cart/>
-      <Fouter/>   
+      <BrowserRouter>
+        <Header/>
+        <Route path='/' component={Cart} /> 
+        <Route path='/register' component={Register} /> 
+        <Route path='/login' component={Login} /> 
+        <Fouter/>
+      </BrowserRouter>
+       
     </div>
   );
 }
