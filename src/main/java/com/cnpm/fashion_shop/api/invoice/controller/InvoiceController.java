51c3ce89258d2e11d630f7c75c5a9f5d29a16cf7
quangnhan1111 <quangnhan1111@users.noreply.github.com>
2021-03-21 12:13:03 +0700

    dont do anything in invoice

--- src/main/java/com/cnpm/fashion_shop/api/invoice/controller/InvoiceController.java
+++ src/main/java/com/cnpm/fashion_shop/api/invoice/controller/InvoiceController.java
@@ -18,47 +18,47 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping(path = "/api/v1/invoice")
 public class InvoiceController {
-    @Autowired
-    private BrandService brandService;
-
-    @ApiOperation(value = "Get all brands", authorizations = {@Authorization(value = SecurityConstants.SECURITY_JWT_NAME)})
-    @GetMapping
-    public PaginationResponse<BrandResponseDto> getBrand(RequestParamsForGettingList requestParamsForGettingList) {
-        Page<BrandResponseDto> data = brandService.findAllBrandDetails(requestParamsForGettingList.getPage(),
-                requestParamsForGettingList.getSize(),
-                requestParamsForGettingList.getSort(),
-                requestParamsForGettingList.getSearch());
-
-        return new PaginationResponse<>(data);
-    }
-
-
-    @ApiOperation(value = "Create brand", authorizations = {@Authorization(value = SecurityConstants.SECURITY_JWT_NAME)})
-    @PostMapping
-    public ResponseEntity<Response> createBrandDto(
-            @Valid @RequestBody BrandDto dto
-    ) {
-        return brandService.createBrandDto(dto);
-    }
-
-    @ApiOperation(value = "Update brand", authorizations = {@Authorization(value = SecurityConstants.SECURITY_JWT_NAME)})
-    @PatchMapping("/{id_brand}")
-    public ResponseEntity<Response> updateBrand(
-            @PathVariable("id_brand") Integer id,
-            @Valid @RequestBody BrandDto dto
-    ) {
-        return this.brandService.updateBrandDto(id, dto);
-    }
-
-    @ApiOperation(value = "Delete brand", authorizations = {@Authorization(value = SecurityConstants.SECURITY_JWT_NAME)})
-    @DeleteMapping("/{id_brand}")
-    public ResponseEntity<Response> deleteBrand(@PathVariable("id_brand") Integer id) {
-        return this.brandService.deleteBrandDto(id);
-    }
-
-    @ApiOperation(value = "Get brand by id", authorizations = {@Authorization(value = SecurityConstants.SECURITY_JWT_NAME)})
-    @GetMapping("/{id_brand}")
-    public ResponseEntity getOneBrand(@PathVariable("id_brand") Integer id) {
-        return brandService.getOne(id);
-    }
+//    @Autowired
+//    private BrandService brandService;
+//
+//    @ApiOperation(value = "Get all brands", authorizations = {@Authorization(value = SecurityConstants.SECURITY_JWT_NAME)})
+//    @GetMapping
+//    public PaginationResponse<BrandResponseDto> getBrand(RequestParamsForGettingList requestParamsForGettingList) {
+//        Page<BrandResponseDto> data = brandService.findAllBrandDetails(requestParamsForGettingList.getPage(),
+//                requestParamsForGettingList.getSize(),
+//                requestParamsForGettingList.getSort(),
+//                requestParamsForGettingList.getSearch());
+//
+//        return new PaginationResponse<>(data);
+//    }
+//
+//
+//    @ApiOperation(value = "Create brand", authorizations = {@Authorization(value = SecurityConstants.SECURITY_JWT_NAME)})
+//    @PostMapping
+//    public ResponseEntity<Response> createBrandDto(
+//            @Valid @RequestBody BrandDto dto
+//    ) {
+//        return brandService.createBrandDto(dto);
+//    }
+//
+//    @ApiOperation(value = "Update brand", authorizations = {@Authorization(value = SecurityConstants.SECURITY_JWT_NAME)})
+//    @PatchMapping("/{id_brand}")
+//    public ResponseEntity<Response> updateBrand(
+//            @PathVariable("id_brand") Integer id,
+//            @Valid @RequestBody BrandDto dto
+//    ) {
+//        return this.brandService.updateBrandDto(id, dto);
+//    }
+//
+//    @ApiOperation(value = "Delete brand", authorizations = {@Authorization(value = SecurityConstants.SECURITY_JWT_NAME)})
+//    @DeleteMapping("/{id_brand}")
+//    public ResponseEntity<Response> deleteBrand(@PathVariable("id_brand") Integer id) {
+//        return this.brandService.deleteBrandDto(id);
+//    }
+//
+//    @ApiOperation(value = "Get brand by id", authorizations = {@Authorization(value = SecurityConstants.SECURITY_JWT_NAME)})
+//    @GetMapping("/{id_brand}")
+//    public ResponseEntity getOneBrand(@PathVariable("id_brand") Integer id) {
+//        return brandService.getOne(id);
+//    }
 }
--- src/main/java/com/cnpm/fashion_shop/core/invoice/repository/InvoiceRepository.java
+++ src/main/java/com/cnpm/fashion_shop/core/invoice/repository/InvoiceRepository.java
@@ -14,13 +14,13 @@ import java.util.Optional;
 
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-    @Query(value = "SELECT * FROM brand b WHERE LOWER(b.name) LIKE %:keyword% AND b.is_deleted = FALSE", nativeQuery = true)
-    Page<InvoiceResponseDto> findAllByName(Pageable pageable, @Param("keyword") String keyword);
-
-    Invoice findByName(String name);
-
-    @Query(value = "SELECT * FROM brand b WHERE b.id = :id AND b.is_deleted = FALSE", nativeQuery = true)
-    Optional<Invoice> findById_invoice(@Param("id") Integer id);
+//    @Query(value = "SELECT * FROM brand b WHERE LOWER(b.name) LIKE %:keyword% AND b.is_deleted = FALSE", nativeQuery = true)
+//    Page<InvoiceResponseDto> findAllByName(Pageable pageable, @Param("keyword") String keyword);
+//
+//    Invoice findByName(String name);
+//
+//    @Query(value = "SELECT * FROM brand b WHERE b.id = :id AND b.is_deleted = FALSE", nativeQuery = true)
+//    Optional<Invoice> findById_invoice(@Param("id") Integer id);
 
 
 //    @Query(value = "SELECT p.id,p.name as Name,p.price,p.number,p.des,b.name as Name_Brand,c.name as Name_Category,g.name as Name_Gender,i.name as Name_Image,i.link " +
