05a1de06b3401ce07c1d04a92f0f58fcb1174a2d
quangnhan1111 <quangnhan1111@users.noreply.github.com>
2021-05-11 08:06:59 +0700

    update select in Product Page

--- src/helper/axiosInstance.js
+++ src/helper/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-    baseURL: 'http://192.168.1.165:9090/api/v1/',
+    baseURL: 'http://192.168.1.9:9090/api/v1/',
   });
 
   export default axiosInstance;
