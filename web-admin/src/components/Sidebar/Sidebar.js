9097a16203e5f6ec26383c1fd1bf1f2d68965186
Tran Viet Thanh <vietthanhqt123@gmail.com>
2021-07-24 21:59:42 +0700

    Fix side bar

--- web-admin/src/components/Sidebar/Sidebar.js
+++ web-admin/src/components/Sidebar/Sidebar.js
@@ -18,29 +18,31 @@ function Sidebar() {
     useEffect(() => {
         check.checklogin();
         // const requestUrl = `/invoice/ByCustomer/status?${paramsString}`
+        let roleName = localStorage.getItem("roleNames")
         const apiUrl = '/invoice/ByCustomer/all'
         let count = 0
-        API.get(apiUrl,token).then((response)=> {   
+        if (roleName === "employee") {
+            API.get(apiUrl,token).then((response)=> {   
             
-            console.log(response.data)
-            let dataInvoice = response.data
-            let listIdInvoice = [] 
-            dataInvoice.forEach(element => {
-                if (listIdInvoice.includes(element.id)) {
-                    return;
-                } 
-                listIdInvoice.push(element.id)
-                if (element.is_paid == false) {
-                    count++;
-                }
+                console.log(response.data)
+                let dataInvoice = response.data
+                let listIdInvoice = [] 
+                dataInvoice.forEach(element => {
+                    if (listIdInvoice.includes(element.id)) {
+                        return;
+                    } 
+                    listIdInvoice.push(element.id)
+                    if (element.is_paid == false) {
+                        count++;
+                    }
+                });
+    
+                console.log(listIdInvoice)
+                setCountInvoiceUnPaid(count)
+                
+            }).catch((error) =>{
             });
-
-            console.log(listIdInvoice)
-            setCountInvoiceUnPaid(count)
-            
-        }).catch((error) =>{
-        });
-
+        }
     }, [checkStatusInvoice.isChange]);
     return (
         <div className="left-sidebar" data-sidebarbg="skin5">
